b33ba9618add6af7149b69346252afd3d17e40c0
julylun <julylun.cat@gmail.com>
2024-12-12 21:40:46 +0700

    Upadte database file

--- Database.sql
+++ Database.sql
@@ -7,6 +7,9 @@
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
+CREATE DATABASE IF NOT EXISTS chmscit1;
+USE chmscit1;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
